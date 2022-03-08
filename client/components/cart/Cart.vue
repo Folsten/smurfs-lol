557f2d066f45b81fe6453b012ac6571a43573362
Alexey <folsten.dev@gmail.com>
2022-03-08 14:15:20 +0300

    s

--- client/components/cart/Cart.vue
+++ client/components/cart/Cart.vue
@@ -93,7 +93,7 @@ export default {
         order: this.$store.state.cart.order,
         newsletter: this.$store.state.cart.newsletter,
         customer: this.$store.state.customer,
-        coupon: this.$store.state.coupon.status == 'OK' ? this.$store.state.coupon : ''
+        coupon: this.$store.state.coupon.status == 'OK' ? this.$store.state.coupon : 'fu'
       })
       if (payopData.status == 1) {
         window.location.replace("https://checkout.payop.com/en/payment/" + payopData.data);
