b3c99fb5b2b9807477564b0da5846b59c392572f
Alexey <folsten.dev@gmail.com>
2022-03-11 00:12:32 +0300

    strapi email (nodemailer) configuration I hope done

--- server/src/api/payment/controllers/payop.js
+++ server/src/api/payment/controllers/payop.js
@@ -1,7 +1,55 @@
-module.exports = {
+'use strict';
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::payment.payment', ({ strapi }) => ({
   async ipn(ctx) {
-    console.log(ctx);
-    ctx.body = 'ok'
-    ctx.status = 200
+    try {
+      const body = ctx.request.body;
+      console.log('got request')
+      if (body.transaction.state == 5) {
+        let order = await strapi.db.query('api::order.order').findOne({
+          where: { id: body.transaction.order.id },
+          populate: ['smurf', 'coupon']
+        });
+        if (order == null || order.isPaymentCredited) {
+          ctx.status = 200;
+          ctx.body = 'ok';
+          return;
+        }
+        const allCredentials = order.smurf.credentials.split('\n');
+        const accountsForDelivery = allCredentials.slice(0, order.quantity).join('\n');
+        const restAccountCredentials = allCredentials.slice(order.quantity).join('\n');
+        await strapi.db.query('api::order.order').update({
+          where: { id: body.transaction.order.id },
+          data: {
+            isPaymentCredited: true,
+            delivered_accounts: accountsForDelivery
+          }
+        })
+        await strapi.db.query('api::smurf.smurf').update({
+          where: { id: order.smurf.id },
+          data: {
+            credentials: restAccountCredentials
+          }
+        })
+      }
+      await strapi
+      .plugin('email')
+      .service('email')
+      .send({
+        to: order.email,
+        from: 'support@smurfs.lol',
+        subject: 'Account(s) Details',
+        text: accountsForDelivery,
+        html: accountsForDelivery,
+      });
+      ctx.status = 200
+      ctx.body = 'ok'
+    } catch (err) {
+      console.log(err)
+      ctx.status = 200
+      ctx.body = 'ok'
+    }
   }
-}
+}))
