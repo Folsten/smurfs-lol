749eb5c00c30f7a43da202d95b4dd55b3df79ffd
Alexey <folsten.dev@gmail.com>
2022-03-12 21:32:18 +0300

    added some things and fixed some errors for custompayment page

--- client/pages/CustomPayment.vue
+++ client/pages/CustomPayment.vue
@@ -1,42 +1,73 @@
 <template>
   <div>
+    <!-- <Header></Header> -->
     <div class="h-screen flex items-center justify-center">
-      <div class="w-80 flex justify-center items-center rounded-main">
-        <form @submit.prevent="checkout" class="flex flex-col w-full">
-          <input
-            type="email"
-            required
-            placeholder="Email"
-            v-model="email"
-            class="rounded-main w-full h-10 pl-3 pr-3 text-black"
+      <div class="container flex-col items-center">
+        <div class="sectionTitle">CUSTOM PAYMENT</div>
+        <div class="subTitle mb-4">
+          Special form to generate custom transaction with the specified value
+        </div>
+        <div class="w-80 flex justify-center items-center rounded-main">
+          <form @submit.prevent="checkout" class="flex flex-col w-full">
+            <input
+              type="email"
+              required
+              placeholder="Email"
+              v-model="email"
+              class="rounded-main w-full h-10 pl-3 pr-3 text-black"
+            />
+            <input
+              required
+              type="number"
+              @keypress="numbersOnly"
+              class="mt-3 rounded-main w-full h-10 pl-3 pr-3 text-black"
+              placeholder="Amount"
+              v-model="amountFiltered"
+              @keydown.space.prevent
+            />
+            <select
+              v-model="currency"
+              class="mt-3 text-black h-10 rounded-main pl-3 pr-3 bg-white"
+            >
+              <option>USD</option>
+              <option>EUR</option>
+            </select>
+            <button
+              type="submit"
+              class="mt-3 bg-success duration-200 h-10 font-bold rounded-main"
+            >
+              Continue
+            </button>
+            <div v-if="error" class="text-error mt-3 text-center">
+              {{ error }}
+            </div>
+          </form>
+        </div>
+        <div class="flex justify-center mt-7">
+          <img
+            src="~/assets/img/paymentMethods/creditCards/Visa/Visa-card-dark.png"
+            class="w-14 h-full ml-1 mr-1"
           />
-          <input
-            required
-            type="text"
-            class="mt-3 rounded-main w-full h-10 pl-3 pr-3 text-black"
-            placeholder="Amount"
-            v-model="amountFiltered"
-            @keydown.space.prevent
+          <img
+            src="~/assets/img/paymentMethods/creditCards/MasterCard/MasterCard-dark.png"
+            class="w-14 h-full ml-1 mr-1"
           />
-          <select
-            v-model="currency"
-            class="mt-3 text-black h-10 rounded-main pl-3 pr-3 bg-white"
-          >
-            <option>USD</option>
-            <option>EUR</option>
-          </select>
-          <button
-            type="submit"
-            class="mt-3 bg-success duration-200 h-10 font-bold rounded-main"
-          >
-            Continue
-          </button>
-          <div v-if="error" class="text-error mt-3 text-center">
-            {{ error }}
-          </div>
-        </form>
+          <img
+            src="~/assets/img/paymentMethods/creditCards/Discover/Discover-card-dark.png"
+            class="w-14 h-full ml-1 mr-1"
+          />
+          <img
+            src="~/assets/img/paymentMethods/creditCards/AmericanExpress/AmericanExpress-dark.png"
+            class="w-14 h-full ml-1 mr-1"
+          />
+          <img
+            src="~/assets/img/paymentMethods/paymentSystems/Skrill/Skrill-card-dark.png"
+            class="w-14 h-full ml-1 mr-1"
+          />
+        </div>
       </div>
     </div>
+    <!-- <Footer></Footer> -->
   </div>
 </template>
 
@@ -52,7 +83,8 @@ export default {
     return {
       email: "",
       amount: "",
-      minAmount: "1",
+      minAmount: 1,
+      maxAmount: 500,
       currency: "USD",
       error: false,
     };
@@ -60,29 +92,48 @@ export default {
   computed: {
     amountFiltered: {
       get() {
-        return this.amount.replace(/[^0-9.]/g, "");
+        return this.amount;
       },
       set(value) {
+        this.amount = value;
+        this.error = false;
         if (value < this.minAmount) {
           this.error = `Amount can't be lower than ${this.minAmount} ${this.currency}`;
-        } else {
-          this.error = false;
         }
-        this.amount = value;
+        if (value > this.maxAmount) {
+          this.error = `Amount can't be higher than ${this.maxAmount} ${this.currency}`;
+        }
+        console.log(this.amount);
       },
     },
   },
   methods: {
     async checkout() {
       if (!this.error) {
+        console.log(this.amount);
         let payopData = await this.$axios.$post("/checkout/custom-payment", {
           email: this.email,
           amount: this.amount,
           currency: this.currency,
         });
         if (payopData.status == 1) {
-          window.location.replace("https://checkout.payop.com/en/payment/" + payopData.data);
+          window.location.replace(
+            "https://checkout.payop.com/en/payment/" + payopData.data
+          );
         }
+      }
+    },
+    numbersOnly(evt) {
+      evt = evt ? evt : window.event;
+      var charCode = evt.which ? evt.which : evt.keyCode;
+      if (
+        charCode > 31 &&
+        (charCode < 48 || charCode > 57) &&
+        charCode !== 46
+      ) {
+        evt.preventDefault();
+      } else {
+        return true;
       }
     },
   },
